0e8612f17a9d09d61c25c03164da264e50636add
AaronT123 <p17228395@my365.dmu.ac.uk>
2020-04-07 20:48:22 +0100

    Restarted the project as the database wasnt working

---
+++ FullMVCProject/FullMVCProjectNew/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FullMVCProjectNew.Startup))]
+namespace FullMVCProjectNew
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
