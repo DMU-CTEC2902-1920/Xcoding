ee7cf6eed188eebb086e54fd0027069c058682e4
AaronT123 <p17228395@my365.dmu.ac.uk>
2020-04-08 04:57:24 +0100

    Added the model classes - currently game, genre, review and user

---
+++ FullMVCProject/FullMVCProjectNew/Models/AnReview.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FullMVCProjectNew.Models
+{
+    public class AnReview
+    {
+        //declare as primary key
+        [Key]
+        public virtual int ReviewID { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Text { get; set; }
+        public virtual string Score { get; set; }
+
+        //declare game foreign key
+        public virtual int GameID { get; set; }
+        public virtual AnGame Game { get; set; }
+        //declare user foreign key
+        public virtual int UserID { get; set; }
+        public virtual AnUser User { get; set; }
+        
+    }
+}
