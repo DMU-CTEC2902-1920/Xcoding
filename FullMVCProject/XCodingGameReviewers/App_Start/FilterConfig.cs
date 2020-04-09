bc8479564aa062e499cde0f788ad6f01047dc895
AaronT123 <p17228395@my365.dmu.ac.uk>
2020-04-09 01:19:12 +0100

    Restarted Again

---
+++ FullMVCProject/XCodingGameReviewers/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace XCodingGameReviewers
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
