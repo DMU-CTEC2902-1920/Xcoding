74271e65315140df09fc7ba25991397a11bb4c48
AaronT123 <p17228395@my365.dmu.ac.uk>
2020-04-09 03:05:55 +0100

    Final restart, all models and scaffholding with shared layout links

---
+++ FullMVCProject/XCodingGameReview/Models/ReviewContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace XCodingGameReview.Models
+{
+    public class ReviewContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ReviewContext() : base("name=ReviewContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<XCodingGameReview.Models.Review> Reviews { get; set; }
+    }
+}
